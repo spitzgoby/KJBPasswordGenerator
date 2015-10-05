82ec88d5ef7480fa19506f3e57a2c329a6696d77
Thomas Leu <tomleu@gmail.com>
2015-10-05 11:19:59 -0500

    Change test reporting

--- tests/GeneratePasswordTest.php
+++ tests/GeneratePasswordTest.php
@@ -2,10 +2,8 @@
 
 class GeneratePasswordTest extends PHPUnit_Framework_TestCase {
   public function testPasswordCanBeGenerated() {
-    echo "Testing password";
     $password = KJBPasswordGenerator\PasswordGenerator::generatePassword(null);
-    echo "Finished generating pasword";
-    echo $password;
+    echo "\nPassword: " .$password ."\n";
     $this->assertNotNull($password);
   }
 }
